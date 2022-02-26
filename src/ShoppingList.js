723f2dba567738b7bc04667758f3e624038057b8
git <chainan2010@gmail.com>
2022-02-26 12:49:12 +0700

    commit message

---
+++ src/ShoppingList.js
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+import React from 'react';
+
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>รายการซื้อสินค้าสำหรับ {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+=======
+import React from 'react';
+
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>รายการซื้อสินค้าสำหรับ {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+>>>>>>> ff0caf6 (update all)
+export default ShoppingList;
