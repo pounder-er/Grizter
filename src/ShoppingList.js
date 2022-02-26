aaa1b1de6f4db21c0a3ab8be7e3461487e4f2c25
git <chainan2010@gmail.com>
2022-02-26 13:09:55 +0700

    update

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 class ShoppingList extends React.Component {
@@ -16,23 +15,4 @@ class ShoppingList extends React.Component {
     }
 }
 
-=======
-import React from 'react';
-
-class ShoppingList extends React.Component {
-    render() {
-        return (
-            <div className="shopping-list">
-                <h1>รายการซื้อสินค้าสำหรับ {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
-            </div>
-        );
-    }
-}
-
->>>>>>> ff0caf6 (update all)
 export default ShoppingList;
